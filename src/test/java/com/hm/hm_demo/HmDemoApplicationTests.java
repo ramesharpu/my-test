9e1a695a682374462381181d3cb5cf69e6922b4a
Ramesh A <ramesharpu@gmail.com>
2026-05-12 09:54:12 +0530

    Add files via upload

---
+++ src/test/java/com/hm/hm_demo/HmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hm.hm_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
